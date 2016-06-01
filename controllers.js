abee540c7d289382caa98f53cc56a28d7c1cba3f
grimal <phillip@philliprstafford.com>
2016-06-01 11:51:46 -0400

    Code: Move weather API call out of controller and to service.

--- controllers.js
+++ controllers.js
@@ -15,15 +15,13 @@ weatherApp.controller('homeController', ['$scope', '$location', 'cityService', f
     
 }]);
 
-weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function ($scope, $resource, $routeParams, cityService) {
+weatherApp.controller('forecastController', ['$scope', '$routeParams', 'cityService', 'weatherService', function($scope, $routeParams, cityService, weatherService) {
     
     $scope.city = cityService.city;
     
     $scope.days = $routeParams.days || '2';
     
-    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=bd44f6dd54a77de0c745f29c4f580df7", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
-    
-    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days });
+    $scope.weatherResult = weatherService.GetWeather($scope.city, $scope.days);
     
     $scope.convertToFahrenheit = function(degK) {
         return Math.round((1.8 * (degK - 273)) + 32);
@@ -33,6 +31,5 @@ weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParam
         
         return new Date(dt * 1000);
     };
-
 
 }]);
--- services.js
+++ services.js
@@ -3,6 +3,15 @@
 weatherApp.service('cityService', function() {
     
     this.city = "New York, NY";
-
                 
 });
+
+weatherApp.service('weatherService', ['$resource', function($resource) {
+    
+    this.GetWeather = function(city, days) {
+        var weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=bd44f6dd54a77de0c745f29c4f580df7", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
+    
+    return weatherAPI.get({ q: city, cnt: days });
+    };
+  
+}]);
